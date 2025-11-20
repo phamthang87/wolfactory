b63456119f114d06580dbd81defbb00473731f3c
phamthang87 <phammanhthang87@gmail.com>
2025-11-20 14:59:13 +0700

    Update GameCard.js

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,12 +1,18 @@
+// components/GameCard.js
 import styles from "../styles/GameCard.module.css";
+import Image from "next/image";
 
-export default function GameCard({ img, title, desc }) {
+export default function GameCard({ img = "/resources/game1.png", title = "Untitled", desc = "" }) {
   return (
-    <div className={styles.card}>
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{desc}</p>
-      <button className={styles.viewBtn}>View Game</button>
-    </div>
+    <article className={styles.card}>
+      <div className={styles.media}>
+        <Image src={img} alt={title} layout="fill" objectFit="cover" />
+      </div>
+      <div className={styles.body}>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <button className={styles.cta}>View Game</button>
+      </div>
+    </article>
   );
 }
