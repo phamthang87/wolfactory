f513c4b9c5765573c396883929dc3b292712d73a
phamthang87 <phammanhthang87@gmail.com>
2025-11-20 23:07:05 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-// pages/index.js
-import Head from "next/head";
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import GamesSection from "../components/GamesSection";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Wolfactory | Mobile Game Studio</title>
-      </Head>
-
-      <Navbar />
-      <main>
-        <HeroSection />
-        <GamesSection />
-      </main>
-      <Footer />
+      <HeroSection />
+      <GamesSection />
     </>
   );
 }
